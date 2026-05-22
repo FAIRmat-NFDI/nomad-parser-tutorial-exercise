eb9cf12778f35c120aacdf34e6ea65cccf50b55e
GinzburgLev <ginzburglev95@gmail.com>
2026-05-22 14:06:51 +0200

    fill schema from the dict

--- src/example_plugin_tutorial_method_a/schema_packages/schema_package.py
+++ src/example_plugin_tutorial_method_a/schema_packages/schema_package.py
@@ -2,6 +2,10 @@ from typing import (
     TYPE_CHECKING,
 )
 
+from nomad.datamodel.metainfo.basesections import (
+    CompositeSystemReference,
+    InstrumentReference,
+)
 from nomad.datamodel.metainfo.eln import ELNMeasurement
 
 from example_plugin_tutorial_method_a.schema_packages import ExampleCategory
@@ -15,7 +19,6 @@ if TYPE_CHECKING:
     )
 
 from nomad.config import config
-from nomad.datamodel.data import Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
 from nomad.metainfo import Quantity, SchemaPackage, Section
 
@@ -53,15 +56,43 @@ class ExampleMicroscopyMeasurement(ELNMeasurement):
         description='Microscopy image magnification',
     )
 
+    image_file = Quantity(
+        type=str,
+        description='Microscopy image file.',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.FileEditQuantity),
+    )
+
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         from example_plugin_tutorial_method_a.util.reader import read_xml_to_dict
 
         super().normalize(archive, logger)
-        print('#######1#######')
 
         if self.file is not None:
-            data_dict = read_xml_to_dict(self.file, archive, logger) # type: ignore
-            print(f'Parsed data from xml file: {data_dict}')
+            data_dict_full = read_xml_to_dict(self.file, archive, logger) # type: ignore
+            data_dict = data_dict_full.get('image_metadata', {})
+            if not (isinstance(data_dict, dict) and data_dict.get('@type') == 'microscopy'):
+                logger.warning('Unexpected structure of the xml file')
+                return
+
+            if 'resolution' in data_dict:
+                self.resolution = [float(x) for x in data_dict['resolution'].split('x')]
+            if 'magnification' in data_dict:
+                self.magnification = float(data_dict['magnification'][:-1])
+            if 'sample' in data_dict and isinstance(data_dict['sample'], dict) and 'sample_ID' in data_dict['sample']:
+                sample = data_dict['sample']
+                self.samples = [
+                    CompositeSystemReference(
+                        lab_id=sample['sample_ID'],
+                    )
+                ]
+            if 'deviceName' in data_dict:
+                self.instruments = [
+                    InstrumentReference(
+                        name=data_dict['deviceName'],
+                    )
+                ]
+            if 'datetime' in data_dict:
+                self.datetime = data_dict['datetime']
 
 
 m_package.__init_metainfo__()
