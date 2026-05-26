2c26a9503292b884816264fb7892fb7023ec57aa
GinzburgLev <ginzburglev95@gmail.com>
2026-05-26 15:01:49 +0200

    add normalization to method b, remove commented code

--- src/example_plugin_tutorial_method_b/parsers/parser.py
+++ src/example_plugin_tutorial_method_b/parsers/parser.py
@@ -21,7 +21,7 @@ from example_plugin_tutorial_method_b.schema_packages.schema_package import (
 )
 
 configuration = config.get_plugin_entry_point(
-    'example_plugin_tutorial.parsers:example_microscopy_parser_entry_point'
+    'example_plugin_tutorial_method_b.parsers:example_microscopy_parser_entry_point'
 )
 
 
@@ -36,46 +36,17 @@ class ExampleXMLParser(MatchingParser):
         archive: 'EntryArchive',
         logger: 'BoundLogger',
     ) -> None:
-        # logger.info('ExampleXMLParser.parse')
-        # data_file = mainfile.rsplit('/', maxsplit=1)[-1]
         filename = mainfile.rsplit('/', maxsplit=1)[-1]
         name = filename.split('.')[0]
         logger.info(f' Example XML Parser called {filename}')
 
-        catalytic_reaction = ExampleMicroscopyMeasurement(
-            data_file=filename,
+        measurement_entry = ExampleMicroscopyMeasurement(
+            file=filename,
         )
 
         archive.data = RawFileData(
             measurement=create_archive(
-                catalytic_reaction, archive, f'{name}.archive.json'
+                measurement_entry, archive, f'{name}.archive.json'
             )
         )
         archive.metadata.entry_name = f'{name} data file'
-
-        # file_name = f'{data_file[:-2]}archive.json'
-        # archive.metadata.entry_type = 'RawMetadataFile'
-        # new_empty_entry = IFMModel.m_from_dict(IFMModel.m_def.a_template)
-        # new_empty_entry.method = 'IFM Model'
-
-        # reprocessing_needed = False
-
-        # with archive.m_context.update_entry(
-        #     file_name, write=True, process=False
-        # ) as model_entry:
-        #     if (
-        #         model_entry.get('data') is None
-        #         or model_entry['data'].get('method') != 'IFM Model'
-        #     ):
-        #         model_entry['data'] = new_empty_entry.m_to_dict(with_root_def=True)
-        #         logger.info(f'IFMModel entry {file_name} created.')
-        #         reprocessing_needed = True
-        #     if model_entry['data'].get('file') is None:
-        #         model_entry['data']['file'] = data_file
-        #         reprocessing_needed = True
-
-        # if reprocessing_needed:
-        #     with archive.m_context.update_entry(
-        #         file_name, write=True, process=True
-        #     ) as model_entry:
-        #         pass
--- src/example_plugin_tutorial_method_b/schema_packages/schema_package.py
+++ src/example_plugin_tutorial_method_b/schema_packages/schema_package.py
@@ -78,36 +78,36 @@ class ExampleMicroscopyMeasurement(ELNMeasurement):
 
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        # from example_plugin_tutorial_method_a.util.reader import read_xml_to_dict
+        from util.reader import read_xml_to_dict
 
         super().normalize(archive, logger)
 
-        # if self.file is not None:
-        #     data_dict_full = read_xml_to_dict(self.file, archive, logger) # type: ignore
-        #     data_dict = data_dict_full.get('image_metadata', {})
-        #     if not (isinstance(data_dict, dict) and data_dict.get('@type') == 'microscopy'):
-        #         logger.warning('Unexpected structure of the xml file')
-        #         return
+        if self.file is not None:
+            data_dict_full = read_xml_to_dict(self.file, archive, logger) # type: ignore
+            data_dict = data_dict_full.get('image_metadata', {})
+            if not (isinstance(data_dict, dict) and data_dict.get('@type') == 'microscopy'):
+                logger.warning('Unexpected structure of the xml file')
+                return
 
-        #     if 'resolution' in data_dict:
-        #         self.resolution = [float(x) for x in data_dict['resolution'].split('x')]
-        #     if 'magnification' in data_dict:
-        #         self.magnification = float(data_dict['magnification'][:-1])
-        #     if 'sample' in data_dict and isinstance(data_dict['sample'], dict) and 'sample_ID' in data_dict['sample']:
-        #         sample = data_dict['sample']
-        #         self.samples = [
-        #             CompositeSystemReference(
-        #                 lab_id=sample['sample_ID'],
-        #             )
-        #         ]
-        #     if 'deviceName' in data_dict:
-        #         self.instruments = [
-        #             InstrumentReference(
-        #                 name=data_dict['deviceName'],
-        #             )
-        #         ]
-        #     if 'datetime' in data_dict:
-        #         self.datetime = data_dict['datetime']
+            if 'resolution' in data_dict:
+                self.resolution = [float(x) for x in data_dict['resolution'].split('x')]
+            if 'magnification' in data_dict:
+                self.magnification = float(data_dict['magnification'][:-1])
+            if 'sample' in data_dict and isinstance(data_dict['sample'], dict) and 'sample_ID' in data_dict['sample']:
+                sample = data_dict['sample']
+                self.samples = [
+                    CompositeSystemReference(
+                        lab_id=sample['sample_ID'],
+                    )
+                ]
+            if 'deviceName' in data_dict:
+                self.instruments = [
+                    InstrumentReference(
+                        name=data_dict['deviceName'],
+                    )
+                ]
+            if 'datetime' in data_dict:
+                self.datetime = data_dict['datetime']
 
 
 m_package.__init_metainfo__()
