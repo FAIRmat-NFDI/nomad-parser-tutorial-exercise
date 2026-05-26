b1786744145f8f2f2c62cce25b0cb28a1fb4f716
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-05-26 15:46:35 +0200

    Update utils

--- src/util/reader.py
+++ src/util/reader.py
@@ -1,4 +1,3 @@
-import os
 from typing import TYPE_CHECKING
 
 from nomad.parsing.file_parser.mapping_parser import XMLParser
@@ -9,15 +8,11 @@ if TYPE_CHECKING:
     )
     from structlog.stdlib import BoundLogger
 
+
 def read_xml_to_dict(file: str, archive: 'EntryArchive', logger: 'BoundLogger') -> dict:
     try:
-        upload_id=archive.metadata.upload_id
-        filepath = os.path.join(
-            os.getcwd(),
-            f'.volumes/fs/staging/{upload_id[0:2]}/{upload_id}/raw', # type: ignore
-            file
-        )
-        result = XMLParser(filepath=filepath).to_dict()
+        with archive.m_context.raw_file(file, write=False) as raw_file:
+            result = XMLParser(filepath=raw_file.name).to_dict()
     except Exception as e:
         logger.error(f'Failed to parse xml file: {e}')
         result = {}
--- src/util/utils.py
+++ src/util/utils.py
@@ -1,6 +1,7 @@
-import json
-import os.path
 from typing import TYPE_CHECKING
+
+import numpy as np
+from nomad.utils import hash as nomad_hash
 
 if TYPE_CHECKING:
     from nomad.datamodel.data import (
@@ -9,6 +10,7 @@ if TYPE_CHECKING:
     from nomad.datamodel.datamodel import (
         EntryArchive,
     )
+    from structlog import BoundLogger
 
 
 def get_reference(upload_id: str, entry_id: str) -> str:
@@ -16,28 +18,92 @@ def get_reference(upload_id: str, entry_id: str) -> str:
 
 
 def get_entry_id_from_file_name(file_name: str, archive: 'EntryArchive') -> str:
-    from nomad.utils import hash
-
-    return hash(archive.metadata.upload_id, file_name)
+    return nomad_hash(archive.metadata.upload_id, file_name)
 
 
 def create_archive(
     entity: 'ArchiveSection',
     archive: 'EntryArchive',
     file_name: str,
+    overwrite: bool = False,
 ) -> str:
-  
-    from nomad.datamodel.context import ClientContext
-    
-    entity_entry = entity.m_to_dict(with_root_def=True)
-    if isinstance(archive.m_context, ClientContext):
-        with open(file_name, 'w') as outfile:
-            json.dump({'data': entity_entry}, outfile, indent=4)
-        return os.path.abspath(file_name)
-    if not archive.m_context.raw_path_exists(file_name):
-        with archive.m_context.raw_file(file_name, 'w') as outfile:
-            json.dump({'data': entity_entry}, outfile)
-        archive.m_context.process_updated_raw_file(file_name)
+    if overwrite or not archive.m_context.raw_path_exists(file_name):
+        with archive.m_context.update_entry(
+            file_name, write=True, process=True
+        ) as entry:
+            entry['data'] = entity.m_to_dict(with_root_def=True)
     return get_reference(
-        archive.metadata.upload_id, get_entry_id_from_file_name(file_name, archive) # type: ignore
+        archive.metadata.upload_id, get_entry_id_from_file_name(file_name, archive)
     )
+
+
+def _not_equal(a, b) -> bool:
+    try:
+        comparison = a != b
+    except ValueError:
+        # If the comparison fails, we assume they are not equal
+        return True
+    if isinstance(comparison, np.ndarray):
+        return comparison.any()
+    return comparison
+
+
+def merge_sections(  # noqa: PLR0912
+    section: 'ArchiveSection',
+    update: 'ArchiveSection',
+    logger: 'BoundLogger' = None,
+) -> None:
+    """
+    Unpopulated quantities and subsections in the `section` will be populated with the
+    values from the `update` section.
+    If a quantity is present in both sections but with different values, no change is
+    made.
+    If a repeating subsection is present in both sections, and they are of the same
+    length, the subsections will be merged recursively. Else, no change is made.
+
+    Args:
+        section (ArchiveSection): section to update.
+        update (ArchiveSection): section to update from.
+        logger (BoundLogger, optional): A structlog logger.
+    """
+    if update is None:
+        return
+    if section is None:
+        section = update.m_copy()
+        return
+    if not isinstance(section, type(update)):
+        raise TypeError(
+            'Cannot merge sections of different types: '
+            f'{type(section)} and {type(update)}'
+        )
+    for name, quantity in update.m_def.all_quantities.items():
+        if not update.m_is_set(quantity):
+            continue
+        if not section.m_is_set(quantity):
+            section.m_set(quantity, update.m_get(quantity))
+        elif _not_equal(section.m_get(quantity), update.m_get(quantity)):
+            warning = f'Merging sections with different values for quantity "{name}".'
+            if logger:
+                logger.warning(warning)
+            else:
+                print(warning)
+    for name, sub_section_def in update.m_def.all_sub_sections.items():
+        count = section.m_sub_section_count(sub_section_def)
+        if count == 0:
+            for update_sub_section in update.m_get_sub_sections(sub_section_def):
+                section.m_add_sub_section(sub_section_def, update_sub_section)
+        elif count == update.m_sub_section_count(sub_section_def):
+            for i in range(count):
+                merge_sections(
+                    section.m_get_sub_section(sub_section_def, i),
+                    update.m_get_sub_section(sub_section_def, i),
+                    logger,
+                )
+        elif update.m_sub_section_count(sub_section_def) > 0:
+            warning = (
+                f'Merging sections with different number of "{name}" sub sections.'
+            )
+            if logger:
+                logger.warning(warning)
+            else:
+                print(warning)
