6c874b3e76b7793828ec5af3b05b4d1075317d80
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-06-09 16:33:34 +0200

    Parser tut 2

--- src/nomad_plugin_tutorials/parsers/tutorial_2/parsers/parser.py
+++ src/nomad_plugin_tutorials/parsers/tutorial_2/parsers/parser.py
@@ -1,45 +1,37 @@
-from typing import (
-    TYPE_CHECKING,
-)
-
-from nomad_plugin_tutorials.parsers.utils import create_archive
-
-if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
-    from structlog.stdlib import (
-        BoundLogger,
-    )
+from typing import TYPE_CHECKING
 
 from nomad.parsing.parser import MatchingParser
 
 from nomad_plugin_tutorials.parsers.tutorial_2.schema.schema_package import (
-    ExampleMicroscopyMeasurement,
-    RawFileData,
+    OpticalMicroscopy,
+    RawFileOpticalMicroscopy,
 )
+from nomad_plugin_tutorials.parsers.utils import create_archive
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import EntryArchive
 
 
-class ExampleXMLParser(MatchingParser):
+class OpticalMicroscopyParser(MatchingParser):
     """
-    Parser for matching .xml metadata files and creating instances of IFMModel.
+    Parser for matching the data files from Optical Microscopy and creating
+    `RawFileOpticalMicroscopy` and `OpticalMicroscopy` ELN entries.
+
+    `RawFileOpticalMicroscopy` is the main archive linked to the data file, whereas
+    `OpticalMicroscopy` ELN is an additional entry linked to an `{name}.archive.json`
+    file.
     """
 
     def parse(
-        self,
-        mainfile: str,
-        archive: 'EntryArchive',
-        logger: 'BoundLogger',
+        self, mainfile: str, archive: 'EntryArchive', logger=None, child_archives=None
     ) -> None:
         filename = mainfile.rsplit('/', maxsplit=1)[-1]
         name = filename.split('.')[0]
         logger.info(f' Example XML Parser called {filename}')
 
-        measurement_entry = ExampleMicroscopyMeasurement(
-            metadata_file=filename,
-        )
+        measurement_entry = OpticalMicroscopy(data_file=filename)
 
-        archive.data = RawFileData(
+        archive.data = RawFileOpticalMicroscopy(
             measurement=create_archive(
                 measurement_entry, archive, f'{name}.archive.json'
             )
--- src/nomad_plugin_tutorials/parsers/tutorial_2/schema/__init__.py
+++ src/nomad_plugin_tutorials/parsers/tutorial_2/schema/__init__.py
@@ -1,7 +1,7 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
 
 
-class ExampleMicroscopySchemaEntryPoint(SchemaPackageEntryPoint):
+class MicroscopySchemaEntryPoint(SchemaPackageEntryPoint):
     def load(self):
         from nomad_plugin_tutorials.parsers.tutorial_2.schema.schema_package import (
             m_package,
@@ -10,7 +10,7 @@ class ExampleMicroscopySchemaEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
-example_microscopy_schema_entry_point = ExampleMicroscopySchemaEntryPoint(
-    name='ExampleMicroscopy',
-    description='Example microscopy schema package entry point.',
+microscopy = MicroscopySchemaEntryPoint(
+    name='Parser Tutorial 2: Microscopy Schema',
+    description='Microscopy schema package entry point.',
 )
