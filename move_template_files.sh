74c76149734ee918b65feb493668f177a90158b0
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-06-10 17:22:27 +0200

    chore: cleanup

--- move_template_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! command -v rsync >/dev/null 2>&1; then
-  echo "rsync required, but not installed!"
-  exit 1
-else
-  rsync -avh example_plugin_tutorial/ .
-  rm -rfv example_plugin_tutorial
-fi
